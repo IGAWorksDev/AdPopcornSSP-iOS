b7da2cd03bacfcd546fc8b21222723ad51b6daa5
Odin-AP <odin.song@adpopcorn.com>
2025-10-01 15:24:03 +0900

    AdpopcornSSP v2.10.7 update

--- AdPopcornSSP.xcframework/ios-arm64/AdPopcornSSP.framework/Headers/AdPopcornSSPWKScriptMessageHandler.h
+++ AdPopcornSSP.xcframework/ios-arm64/AdPopcornSSP.framework/Headers/AdPopcornSSPWKScriptMessageHandler.h
@@ -9,11 +9,28 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
+#import "AdPopcornSSPInterstitialAd.h"
+@protocol AdPopcornSSPWKScriptNativeEventDelegate;
+
 @interface AdPopcornSSPWKScriptMessageHandler : NSObject<WKScriptMessageHandler>
 
+@property (nonatomic, weak) id<AdPopcornSSPWKScriptNativeEventDelegate> nativeEventDelegate;
 @property (nonatomic, weak) id<WKScriptMessageHandler> scriptDelegate;
 @property (nonatomic, weak) WKWebView *webView;
+@property (nonatomic, weak) UIViewController *viewController;
 
 - (instancetype)initWithDelegate:(id<WKScriptMessageHandler>)scriptDelegate;
+
+
+@end
+
+
+@protocol AdPopcornSSPWKScriptNativeEventDelegate <NSObject>
+@optional
+/*!
+ @abstract
+ Onnative Event 관련 호출시.
+ */
+- (void)APSSPOnNativeEvent:(NSString *)data;
 
 @end
